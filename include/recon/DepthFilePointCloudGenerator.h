74369544e980c3719398513b3eae48b68d88221d
Iwer Petersen <iwer.petersen@haw-hamburg.de>
2015-03-22 17:10:11 +0000

    PNG depth and color image to point cloud converter. Not yet finished.
    Needs an interface to load camera parameters (intrinsic as well as extrinsic).

--- include/recon/DepthFilePointCloudGenerator.h
+++ include/recon/DepthFilePointCloudGenerator.h
@@ -1,7 +1,33 @@
 #include "AbstractPointCloudGenerator.h"
+#include <pcl/io/image.h>
+#include <pcl/io/image_depth.h>
+#include <pcl/io/vtk_lib_io.h>
+#include <OniCAPI.h>
+#include <stdint.h>
 
 class DepthFilePointCloudGenerator : AbstractPointCloudGenerator {
 
+	/** \brief Basic camera parameters placeholder. */
+	struct CameraParameters
+	{
+		/** fx */
+		double focal_length_x;
+		/** fy */
+		double focal_length_y;
+		/** cx */
+		double principal_point_x;
+		/** cy */
+		double principal_point_y;
+
+		explicit CameraParameters (double initValue)
+			: focal_length_x (initValue), focal_length_y (initValue),
+			principal_point_x (initValue),  principal_point_y (initValue)
+		{}
+
+		CameraParameters (double fx, double fy, double cx, double cy)
+			: focal_length_x (fx), focal_length_y (fy), principal_point_x (cx), principal_point_y (cy)
+		{ }
+	};
 
 public:
 	DepthFilePointCloudGenerator();
@@ -14,5 +40,23 @@ public:
 	void stop() override;
 
 
-	void loadDepthImageFromFile(std::string fileName);
+	void loadDepthImageFromFile(std::string rgbFileName, std::string depthFileName);
+private:
+	CloudPtr convertToXYZRGBPointCloud (const pcl::io::Image::Ptr &image, const pcl::io::DepthImage::Ptr &depth_image);
+
+	std::string rgb_frame_id_;
+	std::string depth_frame_id_;
+	unsigned image_width_;
+	unsigned image_height_;
+	unsigned depth_width_;
+	unsigned depth_height_;
+
+	float rgb_focal_length_;
+	float depth_focal_length_;
+
+	CameraParameters rgb_parameters_;
+	CameraParameters depth_parameters_;
+
+	std::vector<uint8_t> color_resize_buffer_;
+	std::vector<uint16_t> depth_resize_buffer_;
 };
