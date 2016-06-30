3791c11714efc07d119a169d0469dcfdf40bdb9c
Iwer Petersen <iwer.petersen@haw-hamburg.de>
2016-06-30 12:56:21 +0200

    Added PointNormal typedefs

--- include/recon/typedefs.h
+++ include/recon/typedefs.h
@@ -8,9 +8,15 @@ namespace recon
 {
 	typedef pcl::PointXYZRGB PointType;
 	typedef pcl::PointXYZRGBNormal PointNormalType;
+	
 	typedef pcl::PointCloud<PointType> Cloud;
+	typedef pcl::PointCloud<PointNormalType> NormalCloud;
+	
 	typedef Cloud::ConstPtr CloudConstPtr;
 	typedef Cloud::Ptr CloudPtr;
+
+	typedef NormalCloud::ConstPtr NormalCloudConstPtr;
+	typedef NormalCloud::Ptr NormalCloudPtr;
 
 	typedef boost::shared_ptr<std::vector<pcl::Vertices> > TrianglesPtr;
 	typedef boost::shared_ptr<pcl::io::Image> ImagePtr;
