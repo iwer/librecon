5220d18654e2e7083962e5770d0de89e15455af5
Iwer Petersen <iwer.petersen@haw-hamburg.de>
2016-07-13 00:30:42 +0200

    This was slowing down big time

--- src/AbstractPointCloudGenerator.cpp
+++ src/AbstractPointCloudGenerator.cpp
@@ -17,12 +17,12 @@ namespace recon
 	{
 		CloudConstPtr temp_cloud_;
 
-		if(cloud_mutex_.try_lock()) {
+		//if(cloud_mutex_.try_lock()) {
 			//temp_cloud_.swap(cloud_);
 			temp_cloud_ = cloud_;
-			cloud_mutex_.unlock();
+			//cloud_mutex_.unlock();
 			//std::cout << "Got cloud, has points: " << temp_cloud_->size() << std::endl;
-		}
+		//}
 
 		return temp_cloud_;
 	}
